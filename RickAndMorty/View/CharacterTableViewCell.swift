bffb76a1bd0c2bf8e1c1db3908f8e9387641c044
EmreDereli <emredereli35@hotmail.com>
2021-04-17 18:38:36 +0300

    table view cell design

---
+++ RickAndMorty/View/CharacterTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CharacterTableViewCell.swift
+//  RickAndMorty
+//
+//  Created by TOS ANALYTICS on 17.04.2021.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RickAndMorty/View/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by TOS ANALYTICS on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath) as! CharacterTableViewCell
+        
+        return cell
+        
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    
+    
+
+}
+
