128a25d4e9d726fc6487a6c221156f268572b2ab
EmreDereli <emredereli35@hotmail.com>
2021-04-17 18:42:08 +0300

    cahracter model added

---
+++ RickAndMorty/Model/CharacterModel.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterModel.swift
+//  RickAndMorty
+//
+//  Created by TOS ANALYTICS on 17.04.2021.
+//
+
+import Foundation
+
+struct CharacterModel {
+    private var name : String
+    private var status : String
+    private var species : String
+    private var image : String
+}
